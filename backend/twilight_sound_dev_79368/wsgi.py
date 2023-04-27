1b9b2a0b492ef055048628f15e827815e665774e
Crowdbotics <engineering@crowdbotics.com>
2023-04-27 12:31:47 +0000

    Initial commit.

---
+++ backend/twilight_sound_dev_79368/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for twilight_sound_dev_79368 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twilight_sound_dev_79368.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-sound-dev-79368",
+  url: "https://twilight-sound-dev-79368.botics.co",
+  api: "https://twilight-sound-dev-79368.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
